54e0d9b472ed7c36e97d1c44785471c32a2e86ae
Gerald Calderon <gcalderon-2023362@kinal.edu.gt>
2026-07-21 09:44:41 -0600

    Agregue las clases del modelo y la conexion JDBC

--- ProyectoFinal/src/database/database.sql
+++ ProyectoFinal/src/database/database.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE proyecto_final;
 
+USE proyecto_final;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_completo VARCHAR(100) NOT NULL,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    tipo_usuario ENUM('Profesor', 'Estudiante') NOT NULL
+);
